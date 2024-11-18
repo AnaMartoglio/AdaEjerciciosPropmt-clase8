e0de816d38d9f1df4b8412625ddd3c41c7f949c8
MartoglioAnaDev <martoglioana@gmail.com>
2024-11-18 15:59:48 -0300

    Ejercicios1 resueltos

---
+++ ejercicio1.js
@@ -0,0 +1,41 @@
+//Ejercicio 1 - Ir a comprar un alfajor a la esquina
+let plataEnBolsillo = 20000; 
+let tipoDeAlfajor = "chocolate"; 
+let estaAbiertoKiosco = true; 
+let horaActual = 18; 
+let distanciaAlKiosco = 50; 
+let cantidadDeAlfajores = 1; 
+
+
+console.log(Plata en el bolsillo: ${plataEnBolsillo});
+console.log(Tipo de alfajor: ${tipoDeAlfajor});
+console.log(¿Está abierto el kiosco?: ${estaAbiertoKiosco});
+console.log(Hora actual: ${horaActual});
+console.log(Distancia al kiosco: ${distanciaAlKiosco});
+console.log(Cantidad de alfajores: ${cantidadDeAlfajores});
+
+//Ejercicio 2 - Salir a pasear con el perro
+
+let nombreDelPerro = "Rocco"; 
+let correaLista = true; 
+let estaLloviendo = false; 
+let tiempoPaseo = 30; 
+let temperaturaExterior = 20; 
+let cantidadBolsitas = 2; 
+
+console.log(Nombre del perro: ${nombreDelPerro});
+console.log(¿Correa lista?: ${correaLista});
+console.log(¿Está lloviendo?: ${estaLloviendo});
+console.log(Tiempo estimado de paseo: ${tiempoPaseo} minutos);
+console.log(Temperatura exterior: ${temperaturaExterior} °C);
+console.log(Cantidad de bolsitas: ${cantidadBolsitas});
+
+//Diferencia entre las líneas de código
+
+//La diferencia entre las líneas console.log("Esteban") y console.log(nombre)
+ //es que la primera imprime un valor fijo, "Esteban". Eso significa que siempre,
+ //  me va a mostrar "Esteban" en la consola. En cambio, 
+ // console.log(nombre) toma el valor de la variable nombre, que puede cambiar
+  // dependiendo de lo que asigne a esa variable. La segunda opción
+   // (console.log(nombre)) es mucho más flexible y útil, ya que se puede reutilizar 
+    //la misma línea de código con distintos valores.
