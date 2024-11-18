4caea1d232a68d7d102fea5211065da370a400b7
MartoglioAnaDev <martoglioana@gmail.com>
2024-11-18 16:25:58 -0300

    corrección de errores en ejercicio1.js

---
+++ Ejercicio2.js
@@ -0,0 +1,13 @@
+// Pedir al usuario que ingrese tres números
+let numero1 = parseFloat(prompt("Ingresa el primer número:"));
+let numero2 = parseFloat(prompt("Ingresa el segundo número:"));
+let numero3 = parseFloat(prompt("Ingresa el tercer número:"));
+
+// Comparar los tres números para determinar cuál es el mayor
+if (numero1 >= numero2 && numero1 >= numero3) {
+    console.log("El número mayor es: " + numero1);
+} else if (numero2 >= numero1 && numero2 >= numero3) {
+    console.log("El número mayor es: " + numero2);
+} else {
+    console.log("El número mayor es: " + numero3);
+}
--- ejercicio1.js
+++ ejercicio1.js
@@ -1,34 +1,35 @@
 //Ejercicio 1 - Ir a comprar un alfajor a la esquina
-let plataEnBolsillo = 20000; 
-let tipoDeAlfajor = "chocolate"; 
-let estaAbiertoKiosco = true; 
-let horaActual = 18; 
-let distanciaAlKiosco = 50; 
-let cantidadDeAlfajores = 1; 
 
+let plataEnBolsillo = 20000;
+let tipoDeAlfajor = "chocolate";
+let estaAbiertoKiosco = true;
+let horaActual = 18;
+let distanciaAlKiosco = 50;
+let cantidadDeAlfajores = 1;
 
-console.log(Plata en el bolsillo: ${plataEnBolsillo});
-console.log(Tipo de alfajor: ${tipoDeAlfajor});
-console.log(¿Está abierto el kiosco?: ${estaAbiertoKiosco});
-console.log(Hora actual: ${horaActual});
-console.log(Distancia al kiosco: ${distanciaAlKiosco});
-console.log(Cantidad de alfajores: ${cantidadDeAlfajores});
+console.log(`Plata en el bolsillo: ${plataEnBolsillo}`);
+console.log(`Tipo de alfajor: ${tipoDeAlfajor}`);
+console.log(`¿Está abierto el kiosco?: ${estaAbiertoKiosco}`);
+console.log(`Hora actual: ${horaActual}`);
+console.log(`Distancia al kiosco: ${distanciaAlKiosco}`);
+console.log(`Cantidad de alfajores: ${cantidadDeAlfajores}`);
 
 //Ejercicio 2 - Salir a pasear con el perro
 
-let nombreDelPerro = "Rocco"; 
-let correaLista = true; 
-let estaLloviendo = false; 
-let tiempoPaseo = 30; 
-let temperaturaExterior = 20; 
-let cantidadBolsitas = 2; 
+let nombreDelPerro = "Rocco";
+let correaLista = true;
+let estaLloviendo = false;
+let tiempoPaseo = 30;
+let temperaturaExterior = 20;
+let cantidadBolsitas = 2;
 
-console.log(Nombre del perro: ${nombreDelPerro});
-console.log(¿Correa lista?: ${correaLista});
-console.log(¿Está lloviendo?: ${estaLloviendo});
-console.log(Tiempo estimado de paseo: ${tiempoPaseo} minutos);
-console.log(Temperatura exterior: ${temperaturaExterior} °C);
-console.log(Cantidad de bolsitas: ${cantidadBolsitas});
+console.log(`Nombre del perro: ${nombreDelPerro}`);
+console.log(`¿Correa lista?: ${correaLista}`);
+console.log(`¿Está lloviendo?: ${estaLloviendo}`);
+console.log(`Tiempo estimado de paseo: ${tiempoPaseo} minutos`);
+console.log(`Temperatura exterior: ${temperaturaExterior} °C`);
+console.log(`Cantidad de bolsitas: ${cantidadBolsitas}`);
+
 
 //Diferencia entre las líneas de código
 
